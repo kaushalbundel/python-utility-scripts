dd114493b6088b881f25da86bec9101670f48b88
Kaushal Bundel <kaushalbundel@gmail.com>
2026-03-26 19:55:43 +0530

    Feature add: user specified file name

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Info
 
 Contains small python scripts for better life.
+
+1. file-create-date-time.py: Creates simple file with a given file name or with a file name with desired extension. Primarily used for managable notes.
--- file-create-date-time.py
+++ file-create-date-time.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
 '''
+2026-03-25:
+
 A small script for creating a simple text file so that daily task logging can be done in a simple manner.
 
 Spec:
 - Asks for a file extension
 - creates a file in an existing folder
 - Nomenclature of the file is yyyy-mm-dd.[extension]
+
+2026-03-26
+
+- This needs to be more generic like when I provide a file name then the script should accept that, if not then it should work as it currently does. 
 '''
 import datetime
 
 def create_dated_file():
     extension: str = input("Enter the desired file-extension(default is .md): ").strip() or ".md"
+    name: str|None = input("Please provide a name for the file (Default name is current date): ").strip() or None
 
     # remove the leading dot in case extension is typed with a dot
     if extension.startswith("."):
@@ -21,7 +28,10 @@ def create_dated_file():
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
     #creating file name
-    file_name = f"{current_date}.{extension}"
+    if name == None:
+        file_name = f"{current_date}.{extension}"
+    else:
+        file_name = f"{name}.{extension}"
     
     #file creation
     try:
